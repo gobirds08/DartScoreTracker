c42d3cfbf75c84ea83e910071a6166aadb2e769e
gobirds08 <brendankenney3@gmail.com>
2025-03-16 13:24:52 -0400

    first

---
+++ Dart Score Tracker/Dart_Score_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dart_Score_TrackerApp.swift
+//  Dart Score Tracker
+//
+//  Created by Brendan Kenney on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Dart_Score_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
